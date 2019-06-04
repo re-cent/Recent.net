d60a63290d5c57c39dcc714d790b889421071e0a
Alexandros Kritikos <akritikos@outlook.com>
2019-06-04 16:29:15 +0300

    Persists version changes

--- version.ps1
+++ version.ps1
@@ -1,8 +1,8 @@
 if($Env:Build.SourceBranchName -eq "develop"){
 	$PreRelease = $Env:Version+"-dev"
-	Write-Host "##vso[task.setvariable variable=Version]$(PreRelease)"
+	echo "##vso[task.setvariable variable=Version]$(PreRelease)"
 }
 if($Env:Build.SourceBranchName -eq "merge"){
 	$PreRelease = $Env:Version+"-beta"
-	Write-Host "##vso[task.setvariable variable=Version]$(PreRelease)"
+	echo "##vso[task.setvariable variable=Version]$(PreRelease)"
 }
