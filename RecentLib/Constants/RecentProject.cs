5b0c05c27ecf920a1c770f831e4cf94c629fbe2b
jzarifis <giannis@aegeantaxi.com>
2020-11-20 14:08:28 +0200

    Change relayers smart contract

--- RecentLib/Constants/RecentProject.cs
+++ RecentLib/Constants/RecentProject.cs
@@ -10,7 +10,11 @@ namespace RecentLib.Constants
         public static string BlockhainName = "Recent";
         public static string NodeUrl = "http://127.0.0.1:8545";
         public static string ProfileContract = "0xA91e33aD760407bB9582287D99Ef13B6a528aCa3";
-        public static string PaymentChannelsContract = "0x9d5f44d379B0f0bD9b37988690D6DaFE2572659E";
+
+        //public static string PaymentChannelsContract = "0x9d5f44d379B0f0bD9b37988690D6DaFE2572659E";
+
+        public static string PaymentChannelsContract = "0x6875E6846fa34057019e0f9BFb0CEad0F0D5742A";
+
         public static string ValidatorsContract = "0xEdBDCC6429Ee17A4CBB04D247c3FDa842b5a2dA4";
         public static string BlockRewardContract= "0xEdBDCC6429Ee17A4CBB04D247c3FDa842b5a2dA4";
 
